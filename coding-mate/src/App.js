37baf79595e8b68ad15de3a90db0807b27099644
Subin <rla0010101@gmail.com>
2023-10-09 22:38:45 +0900

    Feat: 스토리 및 코드 페이지 ui/ux 및 기능 구현

--- coding-mate/src/App.js
+++ coding-mate/src/App.js
@@ -7,6 +7,8 @@ import Main from "./routes/Main";
 import Quiz from "./routes/Quiz";
 import Posting from "./routes/Posting";
 import Guide from "./routes/Guide";
+import Story from "./routes/Story";
+import Code from "./routes/Code";
 import "./App.css";
 
 export default function App() {
@@ -17,7 +19,8 @@ export default function App() {
           <Route path="/" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="pwchange" element={<PwChange />} />
-          <Route path="/main" element={<Main />} />
+          <Route path="main" element={<Story />} />
+          <Route path="/code" element={<Code />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/posting" element={<Posting />} />
           <Route path="/guide" element={<Guide />} />
---
+++ coding-mate/src/routes/Story.js
@@ -0,0 +1,10 @@
+import StoryBox from "../components/StoryBox";
+
+
+export default function Story() {
+    return (
+        <div>
+          <StoryBox />
+        </div>
+      );
+}
